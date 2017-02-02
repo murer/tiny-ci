7ef2f0bc605e3ecbd242dad3f763278e1c6dbd86
murer <murer@dextra-sw.com>
2017-02-02 10:32:56 -0200

    init

--- tiny-ci-api/tinyciapi/github.py
+++ tiny-ci-api/tinyciapi/github.py
@@ -1,10 +1,11 @@
 import webutil
 import logging as LOG
+from jsonutil import JSON
 
 class WebhookHandler(webutil.RequestHandler):
     def post(self):
         LOG.info('remove: %s' % (self.request.remote_addr))
         LOG.info('headers: %s' % (self.request.headers))
         body = self.read_json()
-        LOG.info('content: %s' % (body))
+        LOG.info('content: %s' % (JSON.stringify(body, indent=True)))
         self.send_json("OK1")
--- tiny-ci-api/tinyciapi/jsonutil.py
+++ tiny-ci-api/tinyciapi/jsonutil.py
@@ -2,8 +2,8 @@ import json
 
 class JsonUtil(object):
 
-    def stringify(self, obj):
-        return json.dumps(obj)
+    def stringify(self, obj, indent=False):
+        return json.dumps(obj, indent=indent)
 
     def parse(self, data):
         return json.loads(data)
