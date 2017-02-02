6ca7d981ee9f184c976823d3ff0eefab5d2e34d3
murer <murer@dextra-sw.com>
2017-02-02 10:00:18 -0200

    init

--- tiny-ci-api/tinyciapi/github.py
+++ tiny-ci-api/tinyciapi/github.py
@@ -1,5 +1,11 @@
 import webutil
+import logging as LOG
 
 class WebhookHandler(webutil.RequestHandler):
-    def get(self):
+    def post(self):
+        LOG.info('querystring: %s' % (self.request.query_string))
+        LOG.info('headers: %s' % (self.request.headers))
+        LOG.info('content: %s' % (self.request.body))
+        LOG.info('POST: %s' % (self.request.POST))
+        LOG.info('GET: %s' % (self.request.GET))
         self.send_json("OK")
