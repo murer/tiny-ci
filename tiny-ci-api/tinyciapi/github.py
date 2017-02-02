7a257a6bfda222da95bf452503fd5a53cad4b7ef
murer <murer@dextra-sw.com>
2017-02-02 15:52:13 -0200

    init

--- tiny-ci-api/tinyciapi/github.py
+++ tiny-ci-api/tinyciapi/github.py
@@ -1,6 +1,9 @@
 import webutil
 import logging as LOG
 from jsonutil import JSON
+from google.appengine.api import taskqueue
+
+# https://api.github.com/meta
 
 class WebhookHandler(webutil.RequestHandler):
     def post(self):
@@ -8,4 +11,13 @@ class WebhookHandler(webutil.RequestHandler):
         LOG.info('headers: %s' % (self.request.headers))
         body = self.read_json()
         LOG.info('content: %s' % (JSON.stringify(body, indent=True)))
-        self.send_json("OK1")
+        task = taskqueue.add(
+            queue_name = 'pushs',
+            method = 'POST',
+            url = '/api/github/consume',
+            headers = {
+                'Content-Type': 'application/json; charset=UTF-8'
+            },
+            payload = JSON.stringify(body))
+        LOG.info('Task created: %s, eta: %s' % (task.name, task.eta))
+        self.send_json(task.name)
