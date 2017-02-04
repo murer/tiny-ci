a703380490cf8d47ad2e9c599569a1141dc33378
murer <murer@dextra-sw.com>
2017-02-04 09:03:27 -0200

    release

--- tiny-ci-api/tinyciapi/httputil.py
+++ tiny-ci-api/tinyciapi/httputil.py
@@ -4,41 +4,13 @@ from jsonutil import JSON
 from urlparse import urlparse
 
 class Error(Exception):
-	"""Exceptions"""
+    """Exceptions"""
 
 def close(obj):
-	try:
-		obj.close()
-	except:
-		""" Done """
-
-def req(method, url, headers = {}, payload = '', expects = [200]):
-    headers['User-Agent'] = headers.get('User-Agent', 'tiny-ci')
-    parsed = urlparse(url)
-    host = parsed.netloc
-    uri = parsed.path
-    if(parsed.query != ''):
-        uri = uri + '?' + parsed.query
-    conn = None
-    if(parsed.scheme == 'https'):
-        conn = httplib.HTTPSConnection(parsed.hostname, parsed.port or 443)
-    else:
-        conn = httplib.HTTPConnection(parsed.hostname, parsed.port or 80)
     try:
-        if method == 'POST' or method == 'PUT':
-            headers['Content-Type'] = headers.get('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8')
-        if isinstance(payload, dict) and headers['Content-Type'] == 'application/x-www-form-urlencoded;charset=UTF-8':
-            payload = urllib.urlencode(payload)
-        if isinstance(payload, dict) and headers['Content-Type'] == 'application/json':
-            payload = JSON.stringify(payload)
-        conn.request(method, uri, payload, headers)
-        response = conn.getresponse()
-        body = response.read()
-        if response.status not in expects:
-            raise Error('Status: %d %s %sri' % (response.status, response.reason, body))
-        return body
-    finally:
-        close(conn)
+        obj.close()
+    except:
+        """ Done """
 
 class Response(object):
     def __init__(self, status, headers, body):
@@ -46,6 +18,9 @@ class Response(object):
         self.raw_headers = headers
         self.body = body
         self.headers = dict(headers)
+
+    def body_json(self):
+        return JSON.parse(self.body) if self.body else None
 
 
 class Request(object):
@@ -83,12 +58,12 @@ class Request(object):
 def __main():
     req = Request('GET', 'https://api.github.com/meta')
     resp = req.execute()
+    print resp
+    print dir(resp)
     print resp.status
     print resp.raw_headers
     print resp.headers['content-type']
-    print resp.body
-    #obj = req('GET', 'https://api.github.com/meta')
-    #print obj
+    print resp.body_json()
 
 if __name__ == '__main__':
     __main()
