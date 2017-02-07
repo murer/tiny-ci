5c40b04ed9944c3f640d4f5f53b686fb6ab37848
murer <murer@dextra-sw.com>
2017-02-07 10:47:26 -0200

    fix boolean

---
+++ tiny-ci-api/tinyciapi/googleauth.py
@@ -0,0 +1,25 @@
+import webutil
+from httputil import Request as HTTP
+
+class OAuthLoginHandler(webutil.RequestHandler):
+    def get(self):
+        self.send_redirect('https://accounts.google.com/o/oauth2/v2/auth', {
+            'response_type': 'code',
+            'scope': 'https://www.googleapis.com/auth/compute',
+            'redirect_uri': '%s/api/googleauth/oauthcallback' % (self.request.host_url),
+            'client_id': '235531131885-ksue4g0hjtfobgh1khtq51q77coetehg.apps.googleusercontent.com',
+            'access_type': 'offline'
+        })
+
+class OAuthCallbackHandler(webutil.RequestHandler):
+    def get(self):
+        code = self.request.GET['code']
+        req = HTTP('POST', 'https://www.googleapis.com/oauth2/v4/token')
+        resp = req.send_form({
+            'client_id': '235531131885-ksue4g0hjtfobgh1khtq51q77coetehg.apps.googleusercontent.com',
+            'client_secret': 'bGxvF2S9EO5gnkjyADrWr-DW',
+            'code': code,
+            'redirect_uri': '%s/api/googleauth/oauthcallback' % (self.request.host_url),
+            'grant_type': 'authorization_code'
+        }).execute()
+        self.send_json(resp.body_json())
--- tiny-ci-api/tinyciapi/main.py
+++ tiny-ci-api/tinyciapi/main.py
@@ -1,6 +1,7 @@
 import webapp2
 import webutil
 import github
+import googleauth
 
 class PingHandler(webutil.RequestHandler):
     def get(self):
@@ -8,8 +9,13 @@ class PingHandler(webutil.RequestHandler):
 
 app = webapp2.WSGIApplication([
     ('/api/ping', PingHandler),
+
     ('/api/github/oauthlogin', github.OAuthLoginHandler),
     ('/api/github/oauthcallback', github.OAuthCallbackHandler),
     ('/api/github/webhook', github.WebhookHandler),
-    ('/api/github/consume', github.ConsumeHandler)
+    ('/api/github/consume', github.ConsumeHandler),
+
+    ('/api/googleauth/oauthlogin', googleauth.OAuthLoginHandler),
+    ('/api/googleauth/oauthcallback', googleauth.OAuthCallbackHandler)
+
 ])
