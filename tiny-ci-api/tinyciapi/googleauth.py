c529bcdf2187a2d917095a454933adb5a212bcdf
murer <murer@dextra-sw.com>
2017-02-07 16:21:18 -0200

    fix boolean

--- tiny-ci-api/tinyciapi/googleauth.py
+++ tiny-ci-api/tinyciapi/googleauth.py
@@ -6,7 +6,7 @@ class OAuthLoginHandler(webutil.RequestHandler):
         self.send_redirect('https://accounts.google.com/o/oauth2/v2/auth', {
             'response_type': 'code',
             'scope': 'https://www.googleapis.com/auth/compute',
-            'redirect_uri': '%s/api/googleauth/oauthcallback' % (self.request.host_url),
+            'redirect_uri': '%s/api/google/oauthcallback' % (self.request.host_url),
             'client_id': '235531131885-ksue4g0hjtfobgh1khtq51q77coetehg.apps.googleusercontent.com',
             'access_type': 'offline'
         })
@@ -19,7 +19,7 @@ class OAuthCallbackHandler(webutil.RequestHandler):
             'client_id': '235531131885-ksue4g0hjtfobgh1khtq51q77coetehg.apps.googleusercontent.com',
             'client_secret': 'bGxvF2S9EO5gnkjyADrWr-DW',
             'code': code,
-            'redirect_uri': '%s/api/googleauth/oauthcallback' % (self.request.host_url),
+            'redirect_uri': '%s/api/google/oauthcallback' % (self.request.host_url),
             'grant_type': 'authorization_code'
         }).execute()
         self.send_json(resp.body_json())
